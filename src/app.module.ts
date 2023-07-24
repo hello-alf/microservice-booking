7227a786b1d10f9f00540e5c25c94c277a63df0a
Luis Alfredo <alf.182.u@gmail.com>
2023-07-24 08:23:06 -0400

    Actualizando modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SharedKernelModule } from './shared-kernel/shared-kernel.module';
+import { BookingModule } from './booking/booking.module';
 
 @Module({
-  imports: [SharedKernelModule],
+  imports: [SharedKernelModule, BookingModule],
   controllers: [AppController],
   providers: [AppService],
 })
