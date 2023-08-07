22a69ef1b7e64896c1317f7fb8b92c7700f9bc6c
Luis Alfredo <alf.182.u@gmail.com>
2023-08-07 09:20:25 -0400

    Actualizando archivos

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { MongooseModule } from '@nestjs/mongoose';
-import {
-  BookingModelSchema,
-  BookingSchema,
-} from './infrastructure/mongoose/schemas/booking.schema';
 import { CommandHandlers } from './application/commands/handlers';
 import { QueryHandlers } from './application/queries/handlers';
 import { BookingController } from './api/booking/booking.controller';
 import { BookingRepository } from './infrastructure/mongoose/repositories/booking.repository';
+import {
+  BookingModelSchema,
+  BookingSchema,
+} from './infrastructure/mongoose/schemas/booking.schema';
 
 @Module({
   imports: [
